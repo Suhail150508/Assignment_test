e5f189dbdce4861a9d83472e611e521e75a26a88
Suhail150508 <sohail1054155@gmail.com>
2024-03-29 16:29:25 +0600

    Commited

--- routes/api.php
+++ routes/api.php
@@ -8,12 +8,8 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
 */
+
 // header("Access-Control-Allow-Origin", "*");
 // header("Access-Control-Allow-Credentials", "true");
 // header("Access-Control-Max-Age", "1800");
